53bdb211cdf4780b65878f0675b2542737665624
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-06-11 15:17:17 +0300

    Update Task2.java
    w/decomposition

--- Task2.java
+++ Task2.java
@@ -5,31 +5,43 @@ import java.util.Scanner;
 public class Task2 {
 
 	public static int[] mas;
+	public static int n, k;
+	
 
 	public static void main (String [] args) {
-		int n=0, k=0, sum=0;
-		
+		arrayInput(mas);
+			}
+	
+	public static void arrayInput (int[] mas) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Vvedite N=");
 		n = sc.nextInt();
 		System.out.print("Vvedite K=");
 		k = sc.nextInt();
-		int mas[]=new int[n];
-				for (int i=0; i<n; i++) {
-			System.out.print(("ar["+i+"]=>"));
-			mas[i]=sc.nextInt();}
-			for (int i1=0; i1<n; i1++) {
-			System.out.print("mas["+i1+"]="+mas[i1]+"; ");}	
-			
+		int mas1[]=new int[n];
 		for (int i=0; i<n; i++) {
-				if (mas[i]% k == 0) {sum=sum+mas[i];}
-			}
-		
-		System.out.println("sum = " + sum);
-		if (sum==0) {System.out.println("Net chisel kratnyh chisly "+k);}
-				
-							
-			
-			}
+		System.out.print(("ar["+i+"]=>"));
+		mas1[i]=sc.nextInt();}
+		arrayPrint(mas1);
+		findNum(mas1,k);
 	}
+		
+	public static void arrayPrint (int[] mas) {
+		for (int i1=0; i1<n; i1++) {
+			System.out.print("mas["+i1+"]="+mas[i1]+"; ");}	
+		
+	}
+	
+	public static void findNum (int[] mas, int k) {
+		int sum=0;
+		for (int i=0; i<n; i++) {
+			if (mas[i]% k == 0) {sum=sum+mas[i];}
+		}
+	if (sum !=0) {System.out.println("sum = " + sum);}
+	else {System.out.println("Net chisel kratnyh chisly "+k);}
+	}
+	}
+		
+
+	
 	

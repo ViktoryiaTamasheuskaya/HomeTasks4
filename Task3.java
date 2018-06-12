1b2fea757ba37c1a2724c8723613c7c7bc65e4d9
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-06-12 17:16:43 +0300

    Update Task3.java
    w/decomposition

--- Task3.java
+++ Task3.java
@@ -6,41 +6,53 @@ import java.util.Scanner;
 public class Task3 {
 	
 	public static int n;
+	public static int[] mas;
 
 	public static void main (String[] args) {
 		int kol=0;
-		Scanner sc=new Scanner(System.in);
-				System.out.print("Vvedite N=");
-				n = sc.nextInt();
-				int mas[]=new int[n];
-				n=mas.length;
-				Random rand=new Random();
-				for (int i=0; i<n; i++) {
-					mas[i]=rand.nextInt(300);
-					}
-			
-				for (int i=0; i<n; i++) {
-					System.out.println("mas["+i+"]="+mas[i]+"; ");}		
-	
+		arrayInput(mas);
+	}
+		public static void arrayInput(int[] mas) {
+			Scanner sc=new Scanner(System.in);	
+			System.out.print("Vvedite N=");
+			n = sc.nextInt();
+			int mas1[]=new int[n];
+			n=mas1.length;
+			Random rand=new Random();
+			for (int i=0; i<n; i++) {
+				mas1[i]=rand.nextInt(300);
+				}
+			arrayPrint(mas1);
+			arraySort(mas1);
+		}
+		
+		
+		public static void arrayPrint(int[] mas) {
+			for (int i=0; i<n; i++) {
+				System.out.println("mas["+i+"]="+mas[i]+"; ");}	
+		}
+		
+		public static void arraySort(int[] mas) {
+			int kol=0;
+			do {
+				for (int i=0; i<n-1; i++) {
+					if (mas[i]<mas[i+1]) {
+						kol=kol+1;}
+					else {kol=0;
+					System.out.println("massiv ne vozrastaet"); System.exit(0);
+						}
+					}	
 				
-	
-				
-				do {
-					for (int i=0; i<n-1; i++) {
-						if (mas[i]<mas[i+1]) {
-							kol=kol+1;}
-						else {kol=0;
-						System.out.println("massiv ne vozrastaet"); System.exit(0);
-							}
-						}	
-					
-					} while (kol==0);
-				if (kol!=0) {System.out.println("massiv vozrastaet");
-					}
+				} while (kol==0);
+			if (kol!=0) {System.out.println("massiv vozrastaet");
 				}
 			}
+		}
+						
 	
+				
 	
+			
 	
 	
 	
